616d2794c16ca78e71937c162e9c9ce6bbdbc0a4
Riyaz Shaikh <riyazs1305@gmail.com>
2026-04-16 05:59:12 +0530

    skip selenium test in CI

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,14 +1,14 @@
-from selenium import webdriver
+import pytest
 
-
+@pytest.mark.skip(reason="Skip browser test in CI environment")
 def test_google():
+
+    from selenium import webdriver
 
     driver = webdriver.Chrome()
 
     driver.get("https://www.google.com")
 
-    print(driver.title)
+    assert "Google" in driver.title
 
     driver.quit()
-
-    assert True
